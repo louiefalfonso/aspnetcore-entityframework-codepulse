d20656a5668b82f243034234100afd0528d43de0
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-03 13:34:54 +0000

    Added Image FUntionality Domain Model CHanges

--- CodePulse.API/Data/ApplicationDbContext.cs
+++ CodePulse.API/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace CodePulse.API.Data
         public ApplicationDbContext(DbContextOptions options) : base(options) {}
         public DbSet<BlogPost> BlogPosts { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<BlogImage> BlogImages { get; set; }
 
     }
 }
