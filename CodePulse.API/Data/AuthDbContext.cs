c1d6fafda269d8bdaf0e377a584be7331bbde61f
Louie Alfonso <lfriasalfonso@gmail.com>
2025-03-13 16:17:42 +0000

    Setup Authentication & Create DbContext, Seed Roles & Seed Default User

---
+++ CodePulse.API/Data/AuthDbContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePulse.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        //create constructor
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "53eb5141-7eb3-4719-8023-2ff00d7449f2";
+            var writterRoleId ="37718b17-9483-497a-995b-f9388c0288cd";
+
+            // create reader & writter role
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                    ConcurrencyStamp = readerRoleId
+                },
+                 new IdentityRole()
+                {
+                    Id = writterRoleId,
+                    Name = "Writter",
+                    NormalizedName = "Writter".ToUpper(),
+                    ConcurrencyStamp = writterRoleId
+                },
+            };
+            
+            // seed roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // create admin user
+            var adminUserId = "821c4bb9-a23d-4df3-a719-c7cebb7579fa";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "admin@codepluse.com",
+                Email = "admin@codepluse.com",
+                NormalizedEmail = "admin@codepluse.com".ToUpper(),
+                NormalizedUserName = "admin@codepluse.com".ToUpper()
+            };
+
+
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Admin@123");
+
+            builder.Entity<IdentityUser>().HasData(admin);
+
+            // give toles to admin
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = readerRoleId
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = writterRoleId
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+
+        }
+    }
+}
